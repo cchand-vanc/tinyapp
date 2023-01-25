de89584943e41da895256ded1d5aec79abc033b9
cchand-vanc <cmchandler@live.co.uk>
2023-01-24 22:39:11 -0800

    Added cookie-parser packacge so that my username appears next to the log in bar

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 const { generateRandomString } = require("./generateRandomString");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs")
 
@@ -28,7 +30,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -48,7 +50,7 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/:id", (req, res) => { 
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -69,6 +71,8 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL
   res.redirect("/urls");
 });
+
+//Gets cookie
 
 //Handles a POST to /login 
 app.post('/login', (req, res) => {
