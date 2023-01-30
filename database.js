7f595c7f641ab63d7eafd773c15da68b6daee8c0
cchand-vanc <cmchandler@live.co.uk>
2023-01-30 13:07:48 -0800

    Moved user and urlDatabse variables into separate file called database.js

---
+++ database.js
@@ -0,0 +1,16 @@
+const urlDatabase = {};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "b@b",
+    password: "n",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "c@c",
+    password: "v",
+  },
+};
+
+module.exports = {urlDatabase, users};
